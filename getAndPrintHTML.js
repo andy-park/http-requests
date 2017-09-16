bb066e40a82277cf34aa90a0911e280f9c76a8ee
Andrew Park <andresparque@gmail.com>
2017-09-16 19:04:42 +0000

    Changed host and path

--- getAndPrintHTML.js
+++ getAndPrintHTML.js
@@ -4,7 +4,7 @@ function getAndPrintHTML(rawData) {
 
   var requestOptions = {
     host: 'sytantris.github.io',
-    path: '/http-examples/step1.html'
+    path: '/http-examples/step2.html'
   };
 
   // /* Add your code here */
